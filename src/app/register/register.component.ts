e6fef7803ad250c946cc314a02e0193ff0c7f0db
Johan Moreno <johan.moreno01@uceva.edu.co>
2023-10-28 13:50:26 -0500

    Conexión con el registro

--- src/app/register/register.component.ts
+++ src/app/register/register.component.ts
@@ -1,26 +1,29 @@
 import { Component } from '@angular/core';
-import { AuthService } from '../auth.service';
+import { HttpClient } from '@angular/common/http';
 
 @Component({
   selector: 'app-register',
   templateUrl: './register.component.html',
   styleUrls: ['./register.component.css']
 })
-export class RegisterComponent {
-  email: string = '';
-  password: string = '';
-  constructor(private authService: AuthService) { }  
 
-  register() {
-    this.authService.register(this.email, this.password).subscribe(
-      (response: any) => {
-        console.log(response);
-        alert('Usuario registrado con éxito');
-      },   
-      (Error: any) => {
-        console.error(Error);
-        alert('Error al registrar el usuario');
-      }
-    );
+export class RegisterComponent {
+  correo!: string;  // nombre de la propiedad actualizado a 'correo'
+  contrasena!: string;  // nombre de la propiedad actualizado a 'contrasena'
+
+  constructor(private http: HttpClient) { }
+
+  register(): void {
+    const data = {
+      email: this.correo,  // asegúrate de que este campo coincida con lo que espera tu backend
+      password: this.contrasena  // asegúrate de que este campo coincida con lo que espera tu backend
+    };
+
+    this.http.post('http://127.0.0.1:8000/register', data)
+      .subscribe(response => {
+        console.log('Registro exitoso:', response);
+      }, error => {
+        console.error('Error en el registro:', error);
+      });
   }
 }

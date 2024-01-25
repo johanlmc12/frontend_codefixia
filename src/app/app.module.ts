b37e7d9697d95424a46605bbfc7eec5ea6dd5074
farid <farid.avil01@uceva.edu.co>
2024-01-24 23:05:52 -0500

    Correcciones 5

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,14 +7,14 @@ import { RegisterComponent } from './register/register.component';
 import { LoginComponent } from './login/login.component';
 import { AppComponent } from './app.component';
 import { HttpClientModule } from '@angular/common/http';
-import { RouterModule, Routes } from "@angular/router";
-import { AuthService } from "./auth.service";
+import {RouterModule, Routes} from "@angular/router";
+import {AuthService} from "./auth.service";
 import { PrincipalComponent } from './principal/principal.component';
 import { FormRepositorioComponent } from './form-repositorio/form-repositorio.component';
-import { SocialLoginModule, SocialAuthServiceConfig, GoogleLoginProvider } from 'angularx-social-login';
+
 
 const rutas: Routes = [
-  { path: '', redirectTo: '/login', pathMatch: 'full' },
+  {path: '', redirectTo: '/login', pathMatch: 'full'},
   { path: 'login', component: LoginComponent },
   { path: 'register', component: RegisterComponent },
   { path: 'principal', component: PrincipalComponent },
@@ -28,38 +28,23 @@ const rutas: Routes = [
     RegisterComponent,
     PrincipalComponent,
     FormRepositorioComponent
+   
   ],
   imports: [
     BrowserModule,
     HttpClientModule,
     FormsModule,
-    SocialLoginModule,
     RouterModule.forRoot(rutas),
     RouterModule.forChild([
       {
         path: '',
         component: LoginComponent,
-        data: { shouldDetach: true }
+        data: {shouldDetach: true}
       },
     ]),
   ],
-  providers: [
-    AuthService,
-    {
-      provide: 'SocialAuthServiceConfig',
-      useValue: {
-        autoLogin: false, // Puedes configurar esto según tus necesidades
-        providers: [
-          {
-            id: GoogleLoginProvider.PROVIDER_ID,
-            provider: new GoogleLoginProvider(
-              'Tus-Credenciales-de-API-de-Google' // Reemplaza con tus propias credenciales
-            ),
-          },
-        ],
-      } as SocialAuthServiceConfig,
-    },
-  ],
+  providers: [AuthService],
   bootstrap: [AppComponent]
 })
-export class AppModule {}
+
+export class AppModule { }
